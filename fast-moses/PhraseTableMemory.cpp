6b3db9bc0579fd1bd5a5da365e2d7b859115e8b1
Hieu Hoang <hieuhoang@gmail.com>
2016-11-25 19:17:49 +0000

    start pt

---
+++ fast-moses/PhraseTableMemory.cpp
@@ -0,0 +1,18 @@
+/*
+ * PhraseTableMemory.cpp
+ *
+ *  Created on: 25 Nov 2016
+ *      Author: hieu
+ */
+
+#include "PhraseTableMemory.h"
+
+PhraseTableMemory::PhraseTableMemory() {
+	// TODO Auto-generated constructor stub
+
+}
+
+PhraseTableMemory::~PhraseTableMemory() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ fast-moses/PhraseTableMemory.h
@@ -0,0 +1,29 @@
+/*
+ * PhraseTableMemory.h
+ *
+ *  Created on: 25 Nov 2016
+ *      Author: hieu
+ */
+
+#ifndef PHRASETABLEMEMORY_H_
+#define PHRASETABLEMEMORY_H_
+
+class Node
+{
+
+protected:
+
+};
+
+class PhraseTableMemory
+{
+public:
+	PhraseTableMemory();
+	virtual ~PhraseTableMemory();
+
+
+protected:
+	Node m_root;
+};
+
+#endif /* PHRASETABLEMEMORY_H_ */
