2c5e50842f005f7fcea0abf7b9454c88e2d89adc
Hieu Hoang <hieuhoang@gmail.com>
2016-11-25 18:09:30 +0000

    split set and maps into separate files

---
+++ fast-moses/CUDA/Map.cuh
@@ -0,0 +1,107 @@
+/*
+ * Map.cuh
+ *
+ *  Created on: 25 Nov 2016
+ *      Author: hieu
+ */
+
+#ifndef MAP_CUH_
+#define MAP_CUH_
+
+#include <thrust/pair.h>
+#include "Set.cuh"
+
+template<typename Key, typename Value>
+class ComparePair
+{
+public:
+	typedef thrust::pair<Key, Value> Pair;
+
+	__device__ bool operator()(const Pair &a, const Pair &b)
+	{
+	  return a.first < b.first;
+	}
+
+};
+
+template<typename Key, typename Value, typename Compare >
+class Map : public Set<thrust::pair<Key, Value>, Compare>
+{
+public:
+	typedef thrust::pair<Key, Value> Pair;
+  typedef Set<Pair, Compare> Parent;
+
+  Map(const thrust::host_vector<Pair> &vec)
+	:Parent(vec)
+	{}
+
+  void FindMap(thrust::device_vector<bool> &out, const thrust::device_vector<Pair> &sought) const
+  {
+    out.resize(sought.size());
+
+    thrust::binary_search(thrust::device,
+    									Parent::m_vec.begin(), Parent::m_vec.end(),
+                      sought.begin(), sought.end(),
+                      out.begin(),
+                      Compare());
+
+  }
+
+  void FindMap(thrust::host_vector<bool> &out, const thrust::host_vector<Pair> &sought) const
+  {
+    thrust::device_vector<bool> d_out(sought.size());
+
+    FindMap(d_out, sought);
+    out = d_out;
+  }
+
+  bool FindMap(const Key &sought) const
+  {
+    thrust::host_vector<bool> out(1);
+
+    Pair pair(sought, 0.0f);
+    thrust::device_vector<Pair> d_sought(1, pair);
+
+    FindMap(out, d_sought);
+
+    assert(out.size() == 1);
+    return out[0];
+
+  }
+
+  // assumes there's nothing there. Otherwise it will be a multiset
+  void Insert(const Key &key, const Value &value)
+  {
+    typedef thrust::device_vector<typename Map::Pair> Vec;
+    typedef typename Vec::iterator Iter;
+
+		Pair element(key, value);
+  	Iter iter = thrust::lower_bound(thrust::device,
+  												Parent::m_vec.begin(), Parent::m_vec.end(),
+  												element,
+  												Compare() );
+    Parent::m_vec.insert(iter, element);
+
+  }
+
+  std::string Debug() const
+  {
+    std::ostringstream strm;
+
+    for (size_t i = 0; i < Parent::m_vec.size(); ++i) {
+    	const Pair &pair = Parent::m_vec[i];
+    	const Key &key = pair.first;
+    	const Value &value = pair.second;
+			strm << key << "=" << value << " ";
+    }
+
+    return strm.str();
+  }
+
+
+protected:
+
+};
+
+
+#endif /* MAP_CUH_ */
---
+++ fast-moses/CUDA/Set.cuh
@@ -0,0 +1,109 @@
+/*
+ * Util.cuh
+ *
+ *  Created on: 25 Nov 2016
+ *      Author: hieu
+ */
+
+#ifndef UTIL_CUH_
+#define UTIL_CUH_
+
+#include <sstream>
+#include <iostream>
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/binary_search.h>
+#include <thrust/execution_policy.h>
+
+template<typename T>
+void Print(std::ostream &out, const thrust::host_vector<T> &vec)
+{
+  for (size_t i = 0; i < vec.size(); ++i) {
+    out << vec[i] << " ";
+  }
+  out << std::endl;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+template<typename T, typename Compare = thrust::less<T> >
+class Set
+{
+public:
+  Set()
+  {}
+
+  Set(const thrust::host_vector<T> &vec)
+  :m_vec(vec)
+  {
+
+  }
+
+  void Find(thrust::device_vector<bool> &out, const thrust::device_vector<T> &sought) const
+  {
+    out.resize(sought.size());
+    thrust::binary_search(m_vec.begin(), m_vec.end(),
+                      sought.begin(), sought.end(),
+                      out.begin(),
+                      Compare() );
+  }
+
+  void Find(thrust::host_vector<bool> &out, const thrust::host_vector<T> &sought) const
+  {
+    thrust::device_vector<bool> d_out(sought.size());
+    Find(d_out, sought);
+    out = d_out;
+  }
+
+  bool Find(const T &sought) const
+  {
+    thrust::host_vector<T> d_sought(1, sought);
+    thrust::host_vector<bool> out(1);
+
+    Find(out, d_sought);
+
+    assert(out.size() == 1);
+    return out[0];
+  }
+
+  // assumes there's nothing there. Otherwise it will be a multiset
+  void Insert(const T &val)
+  {
+    typedef typename thrust::device_vector<T>::iterator Iter;
+
+  	Iter iter = thrust::lower_bound(thrust::device,
+  												m_vec.begin(), m_vec.end(),
+  												val,
+  												Compare() );
+    m_vec.insert(iter, val);
+  }
+
+  // assume key is in there. Otherwise will delete the next 1 along!
+  void Erase(const T &val)
+  {
+    typedef typename thrust::device_vector<T>::iterator Iter;
+
+  	Iter iter = thrust::lower_bound(thrust::device,
+  												m_vec.begin(), m_vec.end(),
+  												val,
+  												Compare() );
+    m_vec.erase(iter);
+  }
+
+  std::string Debug() const
+  {
+    std::ostringstream strm;
+    Print<T>(strm, m_vec);
+    return strm.str();
+  }
+
+
+protected:
+  thrust::device_vector<T> m_vec;
+
+};
+
+
+
+
+#endif /* UTIL_CUH_ */
