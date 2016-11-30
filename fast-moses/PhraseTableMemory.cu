6f5189253a01eadc00b47ceb11138900a5a83a83
Hieu Hoang <hieuhoang@gmail.com>
2016-11-30 13:23:04 +0000

    use pointers for m_root

--- fast-moses/PhraseTableMemory.cu
+++ fast-moses/PhraseTableMemory.cu
@@ -83,7 +83,9 @@ Node &Node::AddNode(const std::vector<VOCABID> &words, size_t pos)
 }
 
 /////////////////////////////////////////////////////////////////////////////////
-PhraseTableMemory::PhraseTableMemory() {
+PhraseTableMemory::PhraseTableMemory()
+:m_root(new Node)
+{
 	// TODO Auto-generated constructor stub
 
 }
@@ -111,7 +113,7 @@ void PhraseTableMemory::Load(const std::string &path)
 		*/
 
 		vector<VOCABID> sourceIds = vocab.GetOrCreateIds(toks[0]);
-		Node &node = m_root.AddNode(sourceIds);
+		Node &node = m_root->AddNode(sourceIds);
 
 		TargetPhrase *tp = TargetPhrase::CreateFromString(toks[1]);
 		tp->GetScores().CreateFromString(toks[2]);
