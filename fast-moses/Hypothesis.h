066895d7c759b64c5b36cc08c945c7e9de27b26a
Hieu Hoang <hieuhoang@gmail.com>
2016-12-08 14:16:38 +0000

    debug

--- fast-moses/Hypothesis.h
+++ fast-moses/Hypothesis.h
@@ -1,13 +1,15 @@
 #pragma once
 
-#include "CUDA/Managed.h"
-
 class Manager;
 class TargetPhrase;
 
-class Hypothesis : public Managed
+class Hypothesis
 {
 public:
+
+  __device__
+  Hypothesis() {}
+
   void Init(const Manager &mgr);
   void Init(const Manager &mgr, const TargetPhrase &tp);
 
