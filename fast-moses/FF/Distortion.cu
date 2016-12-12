1ccecaa5ea20f98ba133f118c236771d4915db5f
Hieu Hoang <hieuhoang@gmail.com>
2016-12-12 17:40:13 +0000

    debug

--- fast-moses/FF/Distortion.cu
+++ fast-moses/FF/Distortion.cu
@@ -1,5 +1,6 @@
 #include "Distortion.h"
 #include "../Hypothesis.h"
+#include "../Range.h"
 
 Distortion::Distortion()
 {
@@ -7,8 +8,23 @@ Distortion::Distortion()
 }
 
 __device__
-void Distortion::EvaluateWhenApplied(const Manager &mgr, Hypothesis &hypo)
+void Distortion::EvaluateWhenApplied(const Manager &mgr, Hypothesis &hypo) const
 {
   ScoresUnmanaged &scores = hypo.scores;
 
+  //const Range &currRange = hypo.
+}
+
+__device__
+int Distortion::ComputeDistortionDistance(const Range& prev,
+    const Range& current) const
+{
+  int dist = 0;
+  if (prev.GetNumWordsCovered() == 0) {
+    dist = current.GetStartPos();
+  }
+  else {
+    dist = (int) prev.GetEndPos() - (int) current.GetStartPos() + 1;
+  }
+  return abs(dist);
 }
--- fast-moses/FF/Distortion.h
+++ fast-moses/FF/Distortion.h
@@ -9,13 +9,23 @@
 
 #include "StatefulFeatureFunction.h"
 
+class Range;
+
 class Distortion : public StatefulFeatureFunction
 {
 public:
 	Distortion();
 
   __device__
-	void EvaluateWhenApplied(const Manager &mgr, Hypothesis &hypo);
+	void EvaluateWhenApplied(const Manager &mgr, Hypothesis &hypo) const;
+
+
+protected:
+
+  __device__
+  int ComputeDistortionDistance(const Range& prev,
+      const Range& current) const;
+
 };
 
 
