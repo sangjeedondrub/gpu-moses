d7a6cbefde95ee84db3a3f7dd9993f6250a610a1
Hieu Hoang <hieuhoang@gmail.com>
2016-12-13 16:26:32 +0000

    debug

--- fast-moses/CUDA/Array.h
+++ fast-moses/CUDA/Array.h
@@ -6,6 +6,7 @@
  */
 
 #pragma once
+#include <cuda.h>
 
 template<typename T>
 class Array
@@ -37,6 +38,35 @@ public:
   __device__
   T& operator[](size_t ind)
   { return m_arr[ind]; }
+
+  __device__
+  void resize(size_t newSize)
+  {
+    //std::cerr << "newSize=" << newSize << std::endl;
+    if (newSize > m_maxSize) {
+      T *newArr = (T*) malloc(sizeof(T) * newSize);
+
+      size_t oldSize = m_size;
+      memcpy(newArr, m_arr, sizeof(T) * oldSize);
+
+      free(m_arr);
+      m_arr = newArr;
+
+      m_maxSize = newSize;
+    }
+
+    m_size = newSize;
+  }
+
+  __host__
+  const T Get(size_t ind) const
+  {
+    T ret;
+    cudaMemcpy(&ret, &m_arr[ind], sizeof(T), cudaMemcpyDeviceToHost);
+    cudaDeviceSynchronize();
+    return ret;
+
+  }
 
 protected:
   size_t m_size, m_maxSize;
