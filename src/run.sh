8e84437c6c258d5cca98047ed7c05db59a3707da
Hieu Hoang <hieuhoang@gmail.com>
2016-12-29 21:40:30 +0000

    cleanup

--- src/run.sh
+++ src/run.sh
@@ -5,7 +5,7 @@ MOSES_DIR=/home/hieu/workspace/github/mosesdecoder.perf
 #$MOSES_DIR/bin/moses2 -f moses.ini.gpu -verbose 2 -i in.xml -search-algorithm 1 -cube-pruning-pop-limit 2000 -s 2000 -placeholder-factor 1 -xml-input exclusive
 
 DIR=~/workspace/github/gpu-moses/src
-time $DIR/gpu-moses -f moses.ini.gpu
+time $DIR/gpu-moses -f moses.ini.gpu -i in
 
 #CPUPROFILE=/tmp/prof.out $DIR/gpu-moses -f moses.ini.gpu
 
