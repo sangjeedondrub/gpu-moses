66ef9058f880ab8d8a4ca25017fd219a9572a541
Hieu Hoang <hieuhoang@gmail.com>
2016-12-29 12:41:43 +0000

    nsight

--- src/run.sh
+++ src/run.sh
@@ -1,6 +1,8 @@
 MOSES_DIR=/home/hieu/workspace/github/mosesdecoder.perf
 #$MOSES_DIR/bin/moses -f moses.ini.gpu -verbose 2 -i in
 #$MOSES_DIR/bin/moses2 -f moses.ini.gpu -verbose 2 -i in -n-best-list nbest.txt 100
+#$MOSES_DIR/bin/moses -f moses.ini.gpu -verbose 2 -i in.xml  -search-algorithm 1 -cube-pruning-pop-limit 2000 -s 2000 -placeholder-factor 1 -xml-input exclusive
+#$MOSES_DIR/bin/moses2 -f moses.ini.gpu -verbose 2 -i in.xml -search-algorithm 1 -cube-pruning-pop-limit 2000 -s 2000 -placeholder-factor 1 -xml-input exclusive
 
 DIR=~/workspace/github/gpu-moses/src
 time $DIR/gpu-moses -f moses.ini.gpu
